ba0740b553b17ac5b174c45b1758f27d04a548f8
Frank Hermes <frankhermes@cox.net>
2022-01-13 15:04:06 -0800

    Add serial to drive robot motion.

--- __init__.py
+++ __init__.py
@@ -3,6 +3,10 @@ from mycroft import MycroftSkill, intent_file_handler
 # from mycroft.util.log import LOG
 
 # import requests
+import serial
+
+# open serial port
+ser = serial.Serial(port='/dev/ttyUSB0', baudrate=57600, timeout=1)
 
 
 class MycroftBioloidSkill(MycroftSkill):
@@ -11,6 +15,12 @@ class MycroftBioloidSkill(MycroftSkill):
 
     @intent_file_handler('forward.intent')
     def handle_forward(self, message):
+
+        # send command to bot
+        ser.write(b'\xFF\x55\x01\xFE\x00\xFF')
+        # terminates action at one step
+        ser.write(b'\xFF\x55\x00\xFF\x00\xFF')
+        
         self.speak_dialog('forward')
 
 
